d299ef9632be18a36f0bcba1ab41766c1a92be30
chetin4ever <hetan009@gmail.com>
2022-06-05 18:34:59 +0530

    added home about notFound pages and github token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Footer from "./component/layout/Footer"
 import Navbar from "./component/layout/Navbar"
+import About from "./component/pages/About"
+import Home from "./component/pages/Home"
+import NotFound from "./component/pages/NotFound"
 
 function App() {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
